c8fecfba3f8fd1b9271197d300662b4d05573a33
Sergiu Maier <sergiumihaimaier@yahoo.com>
2023-01-18 11:41:48 +0200

    added new user control

---
+++ TCPClient/TCPClient/Profinet/ProfinetPage.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TCPClient.Profinet
+{
+    partial class ProfinetPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProfinetPage));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.buttonConnect = new TCPClient.CustomControls.CustomButton();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(455, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "test test test";
+            // 
+            // panel7
+            // 
+            this.panel7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel7.BackgroundImage")));
+            this.panel7.Controls.Add(this.panel9);
+            this.panel7.Controls.Add(this.buttonConnect);
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(192, 733);
+            this.panel7.TabIndex = 53;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.White;
+            this.panel9.Location = new System.Drawing.Point(11, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(170, 1);
+            this.panel9.TabIndex = 58;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.Color.Transparent;
+            this.buttonConnect.BackgroundColor = System.Drawing.Color.Transparent;
+            this.buttonConnect.BorderColor = System.Drawing.Color.White;
+            this.buttonConnect.BorderRadius = 10;
+            this.buttonConnect.BorderSize = 1;
+            this.buttonConnect.FlatAppearance.BorderSize = 0;
+            this.buttonConnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(52)))), ((int)(((byte)(63)))));
+            this.buttonConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConnect.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonConnect.ForeColor = System.Drawing.Color.White;
+            this.buttonConnect.Image = ((System.Drawing.Image)(resources.GetObject("buttonConnect.Image")));
+            this.buttonConnect.Location = new System.Drawing.Point(16, 30);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(160, 45);
+            this.buttonConnect.TabIndex = 43;
+            this.buttonConnect.Text = "  Connect";
+            this.buttonConnect.TextColor = System.Drawing.Color.White;
+            this.buttonConnect.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            // 
+            // ProfinetPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.label1);
+            this.Name = "ProfinetPage";
+            this.Size = new System.Drawing.Size(850, 730);
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel7;
+        private Panel panel9;
+        private CustomControls.CustomButton buttonConnect;
+    }
+}
